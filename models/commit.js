7af7b356d61ebeb466b2133ed7aae3cd2849a242
Simeron Taak <simeron03@icloud.com>
2016-12-22 15:45:10 +0000

    the graph now works with the commit messages, and the dates

--- models/commit.js
+++ models/commit.js
@@ -4,6 +4,7 @@ class Commit {
         this.author = obj.author.login;
         this.commitDate = new Date(obj.commit.committer.date);
         this.commitMessage = obj.commit.message;
+        this.sha = obj.sha;
     }
 
 }
--- public/js/graphData.js
+++ public/js/graphData.js
@@ -6,12 +6,39 @@ $(() => {
     })
         .then((data) => {
             console.log(data);
+            let x = [];
+            let y = [];
+
+            var layout = {
+                title: 'Your commit history',
+                xaxis: {
+                    title: 'Commit date'
+                }
+            };
+
+            // for (i in data.length) {
+            //     console.log("hello "+i);
+            // };
+
+            for (var i = 0; i < data.length; i++) {
+                y.push(data[i].commitDate);
+                x.push(data[i].commitMessage);
+            }
+            console.log(x);
+            console.log(y);
             var trace1 = {
-                x: [1, 2, 3, 4],
-                y: [10, 15, 13, 17],
+                x: x,
+                y: y, //data.commitMessage
                 type: 'scatter'
             };
-            var data = [trace1,];
-    Plotly.newPlot('myDiv', data);
+            var dataInfo = [trace1];
+
+            Plotly.newPlot('myDiv', dataInfo);
+            
         });
+
+
 });
+
+
+
