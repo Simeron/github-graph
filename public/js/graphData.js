7924adf7de17f0536a991f7ad8758fb9b1f4fd6b
Simeron Taak <simeron03@icloud.com>
2016-12-22 12:09:02 +0000

    the graph now displays with the demo data

--- public/js/graphData.js
+++ public/js/graphData.js
@@ -6,7 +6,12 @@ $(() => {
     })
         .then((data) => {
             console.log(data);
-            
+            var trace1 = {
+                x: [1, 2, 3, 4],
+                y: [10, 15, 13, 17],
+                type: 'scatter'
+            };
+            var data = [trace1,];
+    Plotly.newPlot('myDiv', data);
         });
-
 });
